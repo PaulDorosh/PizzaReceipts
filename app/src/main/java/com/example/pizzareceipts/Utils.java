36bacb0235685af4f2f04b2d00590c7e2f081c0d
Pavel Doroshenko <dorosh98@icloud.com>
2020-01-11 21:35:39 -0500

    First commit

---
+++ app/src/main/java/com/example/pizzareceipts/Utils.java
@@ -0,0 +1,100 @@
+package com.example.pizzareceipts;
+
+public class Utils {
+
+    public static final String PIZZA_1_TITLE = "Hot and Sweet Soppressata";
+    public static final String PIZZA_1_DESCRIPTION = "Sweet & hot pizza on your table";
+    public static final String PIZZA_1_RECIPE = "Place a rack in lower third of oven and preheat to 525° or as high as oven will go.\n" +
+            "\n" +
+            "Once dough has risen in baking sheet, top with mozzarella, dot pie with tomato sauce, and top with hot and sweet soppressata, fennel, chiles, then Pecorino; drizzle with oil. Bake until golden brown and crisp on bottom and sides, 20–30 minutes.\n" +
+            "Serve topped with fennel fronds, sea salt, and red pepper flakes, if using.";
+
+    public static final String PIZZA_2_TITLE = "Ranch Chicken Pizza";
+    public static final String PIZZA_2_DESCRIPTION = "Sweet & hot pizza on your table";
+    public static final String PIZZA_2_RECIPE = "STEP 1\n" +
+            "Preheat oven to 450°F.\n" +
+            "STEP 2\n" +
+            "Spread Ranch dressing evenly on the pizza crust, leaving a ½-inch border around the edges.\n" +
+            "\n" +
+            "STEP 3\n" +
+            "Scatter the chicken, peppers and cheese on top. Bake until cheese is golden and bubbly, about 15 minutes. Let sit for a few minutes before cutting into wedges; serve warm.";
+
+    public static final String PIZZA_3_TITLE = "Ranch Ricotta Pizza";
+    public static final String PIZZA_3_DESCRIPTION = "Sweet & hot pizza on your table";
+    public static final String PIZZA_3_RECIPE = "STEP 1\n" +
+            "Preheat an oven to 450°F. Roll pizza dough out into a 15'' x 9''-inch rectangle and set aside.\n" +
+            "STEP 2\n" +
+            "In a large skillet set over medium heat, add the oil and mushrooms and sauté for 5 minutes, until the mushrooms have softened. Season to taste, with salt.\n" +
+            "STEP 3\n" +
+            "In a large bowl, combine the ricotta cheese and the seasoning mix and stir until well blended. Spread the mixture evenly over the pizza crust, leaving a ½-inch border around the edges. Top with mushroom mixture and mozzarella cheese.\n" +
+            "STEP 4\n" +
+            "Bake for 8 to 10 minutes or until crust is golden and cheese is bubbly. Slice and serve hot.";
+
+    public static final String PIZZA_4_TITLE = "Chicken Ranch Naan Pizza";
+    public static final String PIZZA_4_DESCRIPTION = "Sweet & hot pizza on your table";
+    public static final String PIZZA_4_RECIPE = "STEP 1\n" +
+            "Preheat the oven to 400°F.\n" +
+            "STEP 2\n" +
+            "Spread 2 tablespoons ranch dressing over each piece of naan bread. Add ½ cup sliced chicken breast to each pizza and sprinkle each with ¼ cup mozzarella cheese. Divide sliced tomatoes over each pizza.\n" +
+            "STEP 3\n" +
+            "Bake for 10–12 minutes, or until golden brown and cheese has melted. After pizzas are removed from oven, sprinkle with green onions and Italian parsley.";
+
+    public static final String PIZZA_5_TITLE = "Ranch Buffalo Chicken Pizza";
+    public static final String PIZZA_5_DESCRIPTION = "Sweet & hot pizza on your table";
+    public static final String PIZZA_5_RECIPE = "STEP 1\n" +
+            "Preheat the oven to 425°F.\n" +
+            "STEP 2\n" +
+            "Spoon dressing evenly on the prepared pizza dough, then top with chicken and cheese.\n" +
+            "STEP 3\n" +
+            "Bake for 5 to 7 minutes, or until the cheese has melted and is bubbly. Sprinkle celery over the pizza.\n" +
+            "STEP 4\n" +
+            "Let stand for 2 to 3 minutes before slicing. Serve with extra sauce for dipping, if desired.";
+
+    public static final String PIZZA_6_TITLE = "Salad Pizza";
+    public static final String PIZZA_6_DESCRIPTION = "Sweet & hot pizza on your table";
+    public static final String PIZZA_6_RECIPE = "Mix garlic and 2 Tbsp. oil in a small bowl. Drizzle over dough, using your hands to spread across entire surface; season with salt and pepper. Bake dough, rotating baking sheet once, until golden, puffed, and crisp all over, 10–12 minutes. Let cool slightly (so lettuce won’t wilt).\n" +
+            "\n" +
+            "Toss lettuce, Gorgonzola, pickled onion, and chives in a large bowl; season with salt and pepper. Whisk vinegar and sugar in another small bowl, then, whisking constantly, gradually stream in remaining ⅓ cup oil. Pour dressing over salad and toss to coat.\n" +
+            "\n" +
+            "Arrange salad on top of pizza crust and drape prosciutto over. Slice pizza into squares.";
+
+    public static final String PIZZA_7_TITLE = "Cast-Iron Pizza";
+    public static final String PIZZA_7_DESCRIPTION = "Sweet & hot pizza on your table";
+    public static final String PIZZA_7_RECIPE = "Place a rack in top-most position of oven; preheat to 475°. Place dough on a work surface; drizzle with 1 Tbsp. oil, turning to coat. Stretch out to a 10\" round and cover loosely with plastic wrap.\n" +
+            "\n" +
+            "Heat 1 Tbsp. oil in a large cast-iron skillet over medium. Cook sausage, breaking up into small pieces with a wooden spoon, until browned in spots and cooked though, 5–8 minutes. Transfer sausage to a small bowl.\n" +
+            "\n" +
+            "Remove skillet from heat and carefully lay dough inside (use spoon to help you extend dough all the way to the edges). Season with salt, then spread marinara over entire surface of dough. Top with mozzarella, then fennel, garlic, and cooked sausage. Drizzle with another 2 Tbsp. oil. Peek underneath the crust—the bottom should be golden brown and crisp from residual heat in the skillet. If it’s not, set over medium-low and cook until crust is golden brown, about 3 minutes.\n" +
+            "\n" +
+            "Transfer skillet to oven and bake pizza on top rack until crust is golden brown around the edges and cheese is browned in spots and bubbling all over, 10–14 minutes. Let cool 5 minutes, then top with red pepper flakes and basil. Sprinkle with more salt and drizzle with remaining 1 Tbsp. oil.";
+
+    public static final String PIZZA_8_TITLE = "Herby Pizza with Carrot";
+    public static final String PIZZA_8_DESCRIPTION = "Sweet & hot pizza on your table";
+    public static final String PIZZA_8_RECIPE = "Mix sugar, yeast, and ¾ cup warm water in the bowl of a stand mixer (or in a large bowl if doing by hand) fitted with the dough hook. Stir in flour and 1½ Tbsp. oil. Mix on low speed, scraping down sides of bowl as needed, until dough comes together in a smooth mass and no longer sticks to the sides of bowl, 5–6 minutes. Cover dough and let rest 30 minutes at room temperature. Add salt and continue to knead until fully incorporated into dough.\n" +
+            "\n" +
+            "Turn out dough onto a work surface. Cut dough in half, then shape each half into a ball. Place each ball in separate medium bowls. Drizzle with just enough oil to coat. Cover bowls with plastic wrap. Chill at least 24 hours and up to 2 days.\n" +
+            "\n" +
+            "Let dough come to room temperature until nearly doubled in size and slowly springs back when poked, 2–2½ hours. Dough is now ready for shaping.\n" +
+            "\n" +
+            "Toppings and Assembly\n" +
+            "Place a rack in top third of oven; preheat to 475° (if you have a pizza stone, use it!). Pulse cheese, garlic, lime juice, 1 cup basil, 1 cup carrot tops, ⅓ cup oil, and ¼ cup mint in a food processor until smooth; season with salt. (You can also make the carrot top pesto by chopping everything by hand.)\n" +
+            "\n" +
+            "Stretch dough, 1 ball at a time, on a lightly oiled large rimmed baking sheet until 12–14\" in diameter. Spread with half of the pesto, then top with half of carrots and chiles. Drizzle with more oil; season with salt.\n" +
+            "\n" +
+            "Bake on top rack until crust is golden brown and crisp, 10–12 minutes. Let pizza cool 1 minute, then top with carrot tops, basil, mint, pea shoots and/or blue basil. Serve immediately.";
+
+    public static final String PIZZA_9_TITLE = "Classic Mozzarella Grandma Pie";
+    public static final String PIZZA_9_DESCRIPTION = "Sweet & hot pizza on your table";
+    public static final String PIZZA_9_RECIPE = "Place a rack in lower third of oven and preheat to 525° or as high as oven will go.\n" +
+            "\n" +
+            "Once dough has risen on baking sheet, top with mozzarella, and dot pie with tomato sauce; sprinkle with salt and red pepper flakes, if desired. Bake pie until golden brown and crisp on bottom and sides, 20–30 minutes.";
+
+    public static final String PIZZA_10_TITLE = "Spicy Lamb Pizza";
+    public static final String PIZZA_10_DESCRIPTION = "Sweet & hot pizza on your table";
+    public static final String PIZZA_10_RECIPE = "Place a baking sheet on a rack in lower third of oven; preheat to 500°. Mix garlic, tomato paste, cinnamon, cumin, paprika, and ¾ tsp. salt in a large bowl. Mix in lamb.\n" +
+            "\n" +
+            "Working with 1 piece of dough and keeping remaining piece covered, gently stretch dough into a 10x8\" oval and transfer to a lightly floured parchment-lined baking sheet. (If dough springs back, cover and let rest 10 minutes, then stretch again, resting as needed.) Crumble half of the lamb mixture over dough and brush edges with 1 Tbsp. oil. Slide onto preheated baking sheet and bake until crust is golden brown, 6–8 minutes. Transfer pizza to a wire rack. Repeat with remaining dough, lamb mixture, and 1 Tbsp. oil. Drizzle pizzas with more oil.\n" +
+            "\n" +
+            "Toss onion, parsley, and lemon juice in a bowl; season with salt. Scatter over pizzas; sprinkle with red pepper flakes.";
+
+}
